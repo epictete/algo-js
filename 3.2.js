377295f452b5b51d3b845e844b9ce5d953b03407
John <john.laterre@gmail.com>
2020-06-25 15:44:52 +0200

    3.2

---
+++ 3.2.js
@@ -0,0 +1,13 @@
+let arr1 = [1, 2, 3, 4, 5];
+let sum1 = 0;
+for (let elem of arr1) {
+    sum1 += elem;
+}
+console.log(sum1/arr1.length);
+
+let arr2 = [100, 101, 102];
+let sum2 = 0;
+for (let elem of arr2) {
+    sum2 += elem;
+}
+console.log(sum2/arr2.length);
