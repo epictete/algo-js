08d8937b71c614ae5818446f799166bf6ca2af23
John <john.laterre@gmail.com>
2020-06-30 17:48:45 +0200

    7.3 partiel

---
+++ 7.3.js
@@ -0,0 +1,20 @@
+// Import readlineSync
+const readlineSync = require("readline-sync");
+
+// Ask the user for an integer
+let num = new Number(readlineSync.question('Please enter a positive integer in order to display its divisors : '));
+// console.log(Number.isInteger(num));
+// console.log(num <= 0);
+
+while (num % 1 != 0) {
+    num = new Number(readlineSync.question('You did not read te instructions carefully, please try again : '));
+}
+
+// Function that displays all the divisors except 1 and the number itself
+let div = [];
+for (let i = 1; i < num; i++) {
+    if (num % i == 0 && i != 1) {
+        div.push(i);
+    }
+}
+console.log(div);
