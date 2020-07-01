5a01376a216a10f128752fe70a66443931c0c381
John <john.laterre@gmail.com>
2020-07-01 09:13:56 +0200

    7.3 final

--- 7.3.js
+++ 7.3.js
@@ -3,11 +3,10 @@ const readlineSync = require("readline-sync");
 
 // Ask the user for an integer
 let num = new Number(readlineSync.question('Please enter a positive integer in order to display its divisors : '));
-// console.log(Number.isInteger(num));
-// console.log(num <= 0);
 
-while (num % 1 != 0) {
-    num = new Number(readlineSync.question('You did not read te instructions carefully, please try again : '));
+// Integer validation
+while (num % 1 != 0 || num <= 0) {
+    num = new Number(readlineSync.question('You did not read the instructions carefully, please try again : '));
 }
 
 // Function that displays all the divisors except 1 and the number itself
